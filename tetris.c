1aff3463c0050435f7de4dea1674caaef88f7652
kazu-42 <stu.42Tokyo@gmail.com>
2022-08-09 13:54:43 +0900

    ADD : update_terminal()

--- tetris.c
+++ tetris.c
@@ -114,6 +114,70 @@ void set_timeout(int time) {
 	timeout(1);
 }
 
+void update_terminal(int c)
+{
+	Struct temp = FunctionCS(current);
+	switch(c){
+		case 's':
+			temp.row++;  //move down
+			if(FunctionCP(temp))
+				current.row++;
+			else {
+				int i, j;
+				for(i = 0; i < current.width ;i++){
+					for(j = 0; j < current.width ; j++){
+						if(current.array[i][j])
+							Table[current.row+i][current.col+j] = current.array[i][j];
+					}
+				}
+				int n, m, sum, count=0;
+				for(n=0;n<R;n++){
+					sum = 0;
+					for(m=0;m< C;m++) {
+						sum+=Table[n][m];
+					}
+					if(sum==C){
+						count++;
+						int l, k;
+						for(k = n;k >=1;k--)
+							for(l=0;l<C;l++)
+								Table[k][l]=Table[k-1][l];
+						for(l=0;l<C;l++)
+							Table[k][l]=0;
+						timer-=decrease--;
+					}
+				}
+				final += 100*count;
+				Struct new_shape = FunctionCS(StructsArray[rand()%7]);
+				new_shape.col = rand()%(C-new_shape.width+1);
+				new_shape.row = 0;
+				FunctionDS(current);
+				current = new_shape;
+				if(!FunctionCP(current)){
+					GameOn = F;
+				}
+			}
+			break;
+		case 'd':
+			temp.col++;
+			if(FunctionCP(temp))
+				current.col++;
+			break;
+		case 'a':
+			temp.col--;
+			if(FunctionCP(temp))
+				current.col--;
+			break;
+		case 'w':
+			FunctionRS(temp);
+			if(FunctionCP(temp))
+				FunctionRS(current);
+			break;
+	}
+	FunctionDS(temp);
+	FunctionPT();
+}
+
 int main() {
     srand(time(0));
     final = 0;
@@ -132,128 +196,11 @@ int main() {
     FunctionPT();
 	while(GameOn){
 		if ((c = getch()) != ERR) {
-			Struct temp = FunctionCS(current);
-			switch(c){
-				case 's':
-					temp.row++;  //move down
-					if(FunctionCP(temp))
-						current.row++;
-					else {
-						int i, j;
-						for(i = 0; i < current.width ;i++){
-							for(j = 0; j < current.width ; j++){
-								if(current.array[i][j])
-									Table[current.row+i][current.col+j] = current.array[i][j];
-							}
-						}
-						int n, m, sum, count=0;
-						for(n=0;n<R;n++){
-							sum = 0;
-							for(m=0;m< C;m++) {
-								sum+=Table[n][m];
-							}
-							if(sum==C){
-								count++;
-								int l, k;
-								for(k = n;k >=1;k--)
-									for(l=0;l<C;l++)
-										Table[k][l]=Table[k-1][l];
-								for(l=0;l<C;l++)
-									Table[k][l]=0;
-								timer-=decrease--;
-							}
-						}
-						final += 100*count;
-						Struct new_shape = FunctionCS(StructsArray[rand()%7]);
-						new_shape.col = rand()%(C-new_shape.width+1);
-						new_shape.row = 0;
-						FunctionDS(current);
-						current = new_shape;
-						if(!FunctionCP(current)){
-							GameOn = F;
-						}
-					}
-					break;
-				case 'd':
-					temp.col++;
-					if(FunctionCP(temp))
-						current.col++;
-					break;
-				case 'a':
-					temp.col--;
-					if(FunctionCP(temp))
-						current.col--;
-					break;
-				case 'w':
-					FunctionRS(temp);
-					if(FunctionCP(temp))
-						FunctionRS(current);
-					break;
-			}
-			FunctionDS(temp);
-			FunctionPT();
+			update_terminal(c);
 		}
 		gettimeofday(&now, NULL);
 		if (hasToUpdate()) {
-			Struct temp = FunctionCS(current);
-			switch('s'){
-				case 's':
-					temp.row++;
-					if(FunctionCP(temp))
-						current.row++;
-					else {
-						int i, j;
-						for(i = 0; i < current.width ;i++){
-							for(j = 0; j < current.width ; j++){
-								if(current.array[i][j])
-									Table[current.row+i][current.col+j] = current.array[i][j];
-							}
-						}
-						int n, m, sum, count=0;
-						for(n=0;n<R;n++){
-							sum = 0;
-							for(m=0;m< C;m++) {
-								sum+=Table[n][m];
-							}
-							if(sum==C){
-								count++;
-								int l, k;
-								for(k = n;k >=1;k--)
-									for(l=0;l<C;l++)
-										Table[k][l]=Table[k-1][l];
-								for(l=0;l<C;l++)
-									Table[k][l]=0;
-								timer-=decrease--;
-							}
-						}
-						Struct new_shape = FunctionCS(StructsArray[rand()%7]);
-						new_shape.col = rand()%(C-new_shape.width+1);
-						new_shape.row = 0;
-						FunctionDS(current);
-						current = new_shape;
-						if(!FunctionCP(current)){
-							GameOn = F;
-						}
-					}
-					break;
-				case 'd':
-					temp.col++;
-					if(FunctionCP(temp))
-						current.col++;
-					break;
-				case 'a':
-					temp.col--;
-					if(FunctionCP(temp))
-						current.col--;
-					break;
-				case 'w':
-					FunctionRS(temp);
-					if(FunctionCP(temp))
-						FunctionRS(current);
-					break;
-			}
-			FunctionDS(temp);
-			FunctionPT();
+			update_terminal('s');
 			gettimeofday(&before_now, NULL);
 		}
 	}
