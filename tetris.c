d13ea9b28be04a162a51db40c176b1112d0f9440
kazu-42 <stu.42Tokyo@gmail.com>
2022-08-09 14:33:50 +0900

    REFACTOR : rename to snake_case

--- tetris.c
+++ tetris.c
@@ -8,19 +8,19 @@
 #define ROW 20
 #define COLUMN 15
 
-char Table[ROW][COLUMN] = {0};
-int final = 0;
-bool GameOn = true;
+char g_table[ROW][COLUMN] = {0};
+int g_final = 0;
+bool g_game_on = true;
 suseconds_t timer = 400000;
 int decrease = 1000;
 
 typedef struct {
 	char **array;
 	int width, row, col;
-} Shape;
-Shape current;
+} t_shape;
+t_shape current;
 
-const Shape StructsArray[7] = {
+const t_shape shapes[7] = {
 		{(char *[]) {(char[]) {0, 1, 1}, (char[]) {1, 1, 0}, (char[]) {0, 0, 0}},                                 3},
 		{(char *[]) {(char[]) {1, 1, 0}, (char[]) {0, 1, 1}, (char[]) {0, 0, 0}},                                 3},
 		{(char *[]) {(char[]) {0, 1, 0}, (char[]) {1, 1, 1}, (char[]) {0, 0, 0}},                                 3},
@@ -30,8 +30,8 @@ const Shape StructsArray[7] = {
 		{(char *[]) {(char[]) {0, 0, 0, 0}, (char[]) {1, 1, 1, 1}, (char[]) {0, 0, 0, 0}, (char[]) {0, 0, 0, 0}}, 4}
 };
 
-Shape FunctionCreateShape(Shape shape) {
-	Shape new_shape = shape;
+t_shape copy_shape(t_shape shape) {
+	t_shape new_shape = shape;
 	char **copyshape = shape.array;
 	new_shape.array = (char **) malloc(new_shape.width * sizeof(char *));
 	int i, j;
@@ -44,7 +44,7 @@ Shape FunctionCreateShape(Shape shape) {
 	return new_shape;
 }
 
-void FunctionDestroyShape(Shape shape) {
+void destroy_shape(t_shape shape) {
 	int i;
 	for (i = 0; i < shape.width; i++) {
 		free(shape.array[i]);
@@ -52,7 +52,7 @@ void FunctionDestroyShape(Shape shape) {
 	free(shape.array);
 }
 
-bool FunctionCheckPosition(Shape shape) {
+bool is_valid_position(t_shape shape) {
 	char **array = shape.array;
 	int i, j;
 	for (i = 0; i < shape.width; i++) {
@@ -61,15 +61,15 @@ bool FunctionCheckPosition(Shape shape) {
 				if (array[i][j])
 					return false;
 
-			} else if (Table[shape.row + i][shape.col + j] && array[i][j])
+			} else if (g_table[shape.row + i][shape.col + j] && array[i][j])
 				return false;
 		}
 	}
 	return true;
 }
 
-void FunctionRotateShape(Shape shape) {
-	Shape temp = FunctionCreateShape(shape);
+void rotate_shape(t_shape shape) {
+	t_shape temp = copy_shape(shape);
 	int i, j, k, width;
 	width = shape.width;
 	for (i = 0; i < width; i++) {
@@ -77,16 +77,16 @@ void FunctionRotateShape(Shape shape) {
 			shape.array[i][j] = temp.array[k][i];
 		}
 	}
-	FunctionDestroyShape(temp);
+	destroy_shape(temp);
 }
 
-void FunctionPrint() {
-	char Buffer[ROW][COLUMN] = {0};
+void print_tetris() {
+	char buffer[ROW][COLUMN] = {0};
 	int i, j;
 	for (i = 0; i < current.width; i++) {
 		for (j = 0; j < current.width; j++) {
 			if (current.array[i][j])
-				Buffer[current.row + i][current.col + j] = current.array[i][j];
+				buffer[current.row + i][current.col + j] = current.array[i][j];
 		}
 	}
 	clear();
@@ -95,125 +95,119 @@ void FunctionPrint() {
 	printw("42 Tetris\n");
 	for (i = 0; i < ROW; i++) {
 		for (j = 0; j < COLUMN; j++) {
-			printw("%c ", (Table[i][j] + Buffer[i][j]) ? '#' : '.');
+			printw("%c ", (g_table[i][j] + buffer[i][j]) ? '#' : '.');
 		}
 		printw("\n");
 	}
-	printw("\nScore: %d\n", final);
+	printw("\nScore: %d\n", g_final);
 }
 
 struct timeval before_now, now;
 
-int hasToUpdate() {
+int has_to_update() {
 	return ((suseconds_t)(now.tv_sec * 1000000 + now.tv_usec) -
 			((suseconds_t) before_now.tv_sec * 1000000 + before_now.tv_usec)) > timer;
 }
 
-void set_timeout(int time) {
-	time = 1;
-	timeout(1);
-}
-
 void update_terminal(int c) {
-	Shape temp = FunctionCreateShape(current);
+	t_shape temp = copy_shape(current);
 	switch (c) {
 		case 's':
 			temp.row++;  //move down
-			if (FunctionCheckPosition(temp))
+			if (is_valid_position(temp))
 				current.row++;
 			else {
 				int i, j;
 				for (i = 0; i < current.width; i++) {
 					for (j = 0; j < current.width; j++) {
 						if (current.array[i][j])
-							Table[current.row + i][current.col + j] = current.array[i][j];
+							g_table[current.row + i][current.col + j] = current.array[i][j];
 					}
 				}
 				int n, m, sum, count = 0;
 				for (n = 0; n < ROW; n++) {
 					sum = 0;
 					for (m = 0; m < COLUMN; m++) {
-						sum += Table[n][m];
+						sum += g_table[n][m];
 					}
 					if (sum == COLUMN) {
 						count++;
 						int l, k;
 						for (k = n; k >= 1; k--)
 							for (l = 0; l < COLUMN; l++)
-								Table[k][l] = Table[k - 1][l];
+								g_table[k][l] = g_table[k - 1][l];
 						for (l = 0; l < COLUMN; l++)
-							Table[k][l] = 0;
+							g_table[k][l] = 0;
 						timer -= decrease--;
 					}
 				}
-				final += 100 * count;
-				Shape new_shape = FunctionCreateShape(StructsArray[rand() % 7]);
+				g_final += 100 * count;
+				t_shape new_shape = copy_shape(shapes[rand() % 7]);
 				new_shape.col = rand() % (COLUMN - new_shape.width + 1);
 				new_shape.row = 0;
-				FunctionDestroyShape(current);
+				destroy_shape(current);
 				current = new_shape;
-				if (!FunctionCheckPosition(current)) {
-					GameOn = false;
+				if (!is_valid_position(current)) {
+					g_game_on = false;
 				}
 			}
 			break;
 		case 'd':
 			temp.col++;
-			if (FunctionCheckPosition(temp))
+			if (is_valid_position(temp))
 				current.col++;
 			break;
 		case 'a':
 			temp.col--;
-			if (FunctionCheckPosition(temp))
+			if (is_valid_position(temp))
 				current.col--;
 			break;
 		case 'w':
-			FunctionRotateShape(temp);
-			if (FunctionCheckPosition(temp))
-				FunctionRotateShape(current);
+			rotate_shape(temp);
+			if (is_valid_position(temp))
+				rotate_shape(current);
 			break;
 	}
-	FunctionDestroyShape(temp);
-	FunctionPrint();
+	destroy_shape(temp);
+	print_tetris();
 }
-
 
 int main() {
 	srand(time(0));
-	final = 0;
+	g_final = 0;
 	int c;
 	initscr();
 	gettimeofday(&before_now, NULL);
-	set_timeout(1);
-	Shape new_shape = FunctionCreateShape(StructsArray[rand() % 7]);
+	timeout(1);
+	t_shape new_shape = copy_shape(shapes[rand() % 7]);
 	new_shape.col = rand() % (COLUMN - new_shape.width + 1);
 	new_shape.row = 0;
-	FunctionDestroyShape(current);
+	destroy_shape(current);
 	current = new_shape;
-	if (!FunctionCheckPosition(current)) {
-		GameOn = false;
+	if (!is_valid_position(current)) {
+		g_game_on = false;
 	}
-	FunctionPrint();
-	while (GameOn) {
+	print_tetris();
+	while (g_game_on) {
 		if ((c = getch()) != ERR) {
 			update_terminal(c);
 		}
 		gettimeofday(&now, NULL);
-		if (hasToUpdate()) {
+		if (has_to_update()) {
 			update_terminal('s');
 			gettimeofday(&before_now, NULL);
 		}
 	}
-	FunctionDestroyShape(current);
+	destroy_shape(current);
 	endwin();
 	int i, j;
 	for (i = 0; i < ROW; i++) {
 		for (j = 0; j < COLUMN; j++) {
-			printf("%c ", Table[i][j] ? '#' : '.');
+			printf("%c ", g_table[i][j] ? '#' : '.');
 		}
 		printf("\n");
 	}
 	printf("\nGame over!\n");
-	printf("\nScore: %d\n", final);
+	printf("\nScore: %d\n", g_final);
 	return 0;
 }
