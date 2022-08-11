558c6a2ec47e2c7034d3613e26082aea9e19e43b
Shun Usami <usatie@gmail.com>
2022-08-11 23:32:40 +0900

    :recycle: Refactor using gcc -Weverything

--- Makefile
+++ Makefile
@@ -1,2 +1,19 @@
-tetris: tetris.c
-	gcc tetris.c -lncurses -o tetris
+NAME	=	tetris
+CFLAGS	=	-Wall -Wextra -Werror
+
+$(NAME): tetris.c
+	gcc tetris.c -lncurses -o tetris $(CFLAGS)
+
+.PHONY: all
+all: $(NAME)
+
+.PHONY: clean
+clean:
+	$(RM) *.o
+
+.PHONY: fclean
+fclean: clean
+	$(RM) $(NAME)
+
+.PHONY: re
+re: fclean all
--- tetris.c
+++ tetris.c
@@ -7,9 +7,12 @@
 
 #define ROW 20
 #define COL 15
-#define FRAME_INTERVAL_USEC 400000
+#define DEFAULT_GRAVITY 0.1
+#define ONE_SECOND_IN_USEC 1000000
+#define FRAME_PER_SECOND 30
 #define NUM_TETRIMINOS 7
 #define SCORE_PER_LINE 100
+#define CURSES_READ_INTERVAL_MILLISEC 1
 
 typedef enum {
     MOVE_DOWN,
@@ -28,57 +31,112 @@ typedef char t_board[ROW][COL];
 typedef struct timeval t_timeval;
 
 typedef struct {
-    t_tetromino current;
-    t_board board;
     int score;
     bool game_on;
+    t_board board;
+	double gravity;
+    t_tetromino current;
     t_timeval updated_at;
 } t_context;
 
-const t_tetromino tetrominoes[NUM_TETRIMINOS] = {
+static const t_tetromino tetrominoes[NUM_TETRIMINOS] = {
 	// shape S
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){0, 1, 1},
 			 (char[]){1, 1, 0},
 			 (char[]){0, 0, 0}},
-	 3},
+	 	.length = 3,
+		.row = 0,
+		.col = 0
+	},
 	// shape Z
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){1, 1, 0},
 			 (char[]){0, 1, 1},
 			 (char[]){0, 0, 0}},
-	 3},
+	 	.length = 3,
+		.row = 0,
+		.col = 0
+	},
 	// shape T
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){0, 0, 0},
 			 (char[]){1, 1, 1},
 			 (char[]){0, 1, 0}},
-	 3},
+	 	.length = 3,
+		.row = 0,
+		.col = 0
+	 },
 	// shape L
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){1, 0, 0},
 			 (char[]){1, 0, 0},
 			 (char[]){1, 1, 0}},
-	 3},
+	 	.length = 3,
+		.row = 0,
+		.col = 0
+	 },
 	// shape J
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){0, 0, 1},
 			 (char[]){0, 0, 1},
 			 (char[]){0, 1, 1}},
-	 3},
+	 	.length = 3,
+		.row = 0,
+		.col = 0
+	 },
 	// shape O
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){1, 1},
 			 (char[]){1, 1}},
-	 2},
+	 	.length = 2,
+		.row = 0,
+		.col = 0
+	 },
 	// shape I
-	{(char *[]){
+	{
+		.array = (char *[]){
 			 (char[]){0, 0, 1, 0},
 			 (char[]){0, 0, 1, 0},
 			 (char[]){0, 0, 1, 0},
 			 (char[]){0, 0, 1, 0}},
-	 4}};
+	 	.length = 4,
+		.row = 0,
+		.col = 0
+	 }};
 
+
+t_move to_move(int ch);
+t_tetromino duplicate_tetromino(const t_tetromino piece);
+void destroy_tetromino(t_tetromino piece);
+bool is_valid_position(const t_tetromino piece, const t_board board);
+void rotate_tetromino(t_tetromino *piece);
+void print_tetris(const t_board board, const t_tetromino current, int score);
+int has_to_fall_by_gravity(t_timeval updated_at, double gravity);
+t_tetromino generate_random_tetromino(void);
+void lock_tetromino_to_board(const t_tetromino current, t_board board);
+bool is_line_filled(int row, const t_board board);
+void clear_line(int row, t_board board);
+int	clear_filled_lines(t_board board);
+void move_down(t_tetromino *piece);
+void move_right(t_tetromino *piece);
+void move_left(t_tetromino *piece);
+static void move_piece(t_move move, t_tetromino *piece);
+static bool has_room_to_move(t_move move, const t_tetromino piece, const t_board board);
+void execute(t_move move, t_context *ctx);
+void print_result(int score, const t_board board);
+void init_context(t_context *ctx);
+void init_game(t_context *ctx);
+void loop_game(t_context *ctx);
+void destroy_game(t_context *ctx);
+
+// ---------------------------------
 t_move to_move(int ch) {
     printw("%d\n", ch);
     switch (ch) {
@@ -95,15 +153,15 @@ t_move to_move(int ch) {
     }
 }
 
-t_tetromino duplicate_piece(const t_tetromino piece) {
+t_tetromino duplicate_tetromino(const t_tetromino piece) {
     t_tetromino new_piece = {
-            .array = malloc(sizeof(char *) * piece.length),
+            .array = malloc(sizeof(char *) * (unsigned int)piece.length),
             .length = piece.length,
             .row = piece.row,
             .col = piece.col};
 
     for (int r = 0; r < new_piece.length; r++) {
-        new_piece.array[r] = (char *) malloc(sizeof(char) * new_piece.length);
+        new_piece.array[r] = (char *) malloc(sizeof(char) * (unsigned int)new_piece.length);
         for (int c = 0; c < new_piece.length; c++) {
             new_piece.array[r][c] = piece.array[r][c];
         }
@@ -111,7 +169,7 @@ t_tetromino duplicate_piece(const t_tetromino piece) {
     return new_piece;
 }
 
-void destroy_piece(t_tetromino piece) {
+void destroy_tetromino(t_tetromino piece) {
     for (int r = 0; r < piece.length; r++) {
         free(piece.array[r]);
     }
@@ -135,15 +193,15 @@ bool is_valid_position(const t_tetromino piece, const t_board board) {
 }
 
 // rotate tetromino clockwise
-void rotate_piece(t_tetromino *piece) {//rotate_clockwise
-    t_tetromino temp = duplicate_piece(*piece);
+void rotate_tetromino(t_tetromino *piece) {
+    t_tetromino temp = duplicate_tetromino(*piece);
     for (int i = 0; i < piece->length; i++) {
         for (int j = 0; j < piece->length; j++) {
             int k = piece->length - 1 - j;
             piece->array[i][j] = temp.array[k][i];
         }
     }
-    destroy_piece(temp);
+    destroy_tetromino(temp);
 }
 
 void print_tetris(const t_board board, const t_tetromino current, int score) {
@@ -167,17 +225,20 @@ void print_tetris(const t_board board, const t_tetromino current, int score) {
     printw("\nScore: %d\n", score);
 }
 
-int has_to_update(t_timeval updated_at, suseconds_t frame_interval_usec) {
+// The greater gravity is, the faster falling is.
+// Gravity is expressed in unit G, where 1G = 1 cell per frame, and 0.1G = 1 cell per 10 frames.
+int has_to_fall_by_gravity(t_timeval updated_at, double gravity) {
     t_timeval now;
+	time_t elapsed_usec;
 
     gettimeofday(&now, NULL);
-    suseconds_t elapsed_usec = (now.tv_sec - updated_at.tv_sec) * 1000000 +
+    elapsed_usec = (now.tv_sec - updated_at.tv_sec) * ONE_SECOND_IN_USEC +
                                (now.tv_usec - updated_at.tv_usec);
-    return elapsed_usec > frame_interval_usec;
+	return ((double)elapsed_usec * gravity) > (ONE_SECOND_IN_USEC / FRAME_PER_SECOND);
 }
 
-t_tetromino generate_random_tetromino() {
-    t_tetromino new_piece = duplicate_piece(tetrominoes[rand() % NUM_TETRIMINOS]);
+t_tetromino generate_random_tetromino(void) {
+    t_tetromino new_piece = duplicate_tetromino(tetrominoes[rand() % NUM_TETRIMINOS]);
     new_piece.col = rand() % (COL - new_piece.length + 1);
     new_piece.row = 0;
 	return new_piece;
@@ -248,19 +309,21 @@ static void move_piece(t_move move, t_tetromino *piece) {
             move_left(piece);
             break;
         case MOVE_ROTATE:
-            rotate_piece(piece);
+            rotate_tetromino(piece);
             break;
-        default:
+		case MOVE_UNKNOWN:
             return;
     }
 }
 
 static bool has_room_to_move(t_move move, const t_tetromino piece, const t_board board) {
 	bool	has_room_to_move;
-    t_tetromino temp = duplicate_piece(piece);
+    t_tetromino temp;
+
+	temp = duplicate_tetromino(piece);
 	move_piece(move, &temp);
     has_room_to_move = is_valid_position(temp, board);
-    destroy_piece(temp);
+    destroy_tetromino(temp);
     return has_room_to_move;
 }
 
@@ -273,7 +336,7 @@ void execute(t_move move, t_context *ctx) {
         lock_tetromino_to_board(ctx->current, ctx->board);
 		lines_cleared = clear_filled_lines(ctx->board);
         ctx->score += SCORE_PER_LINE * lines_cleared;
-		destroy_piece(ctx->current);
+		destroy_tetromino(ctx->current);
         ctx->current = generate_random_tetromino();
         if (!is_valid_position(ctx->current, ctx->board)) {
             ctx->game_on = false;
@@ -296,16 +359,22 @@ void init_context(t_context *ctx) {
     *ctx = (t_context){
             .score = 0,
             .game_on = true,
-            .board = {0}};
+            .board = {0},
+			.gravity = DEFAULT_GRAVITY
+	};
 	ctx->current = generate_random_tetromino();
     gettimeofday(&ctx->updated_at, NULL);
 }
 
 void init_game(t_context *ctx) {
+	unsigned int seed;
+
+	// Use current timestamp as randomness seed
+	seed = (unsigned int)time(NULL);
     init_context(ctx);
-    srand(time(NULL));
+    srand(seed);
     initscr();
-    timeout(1);
+    timeout(CURSES_READ_INTERVAL_MILLISEC);
     print_tetris(ctx->board, ctx->current, ctx->score);
 }
 
@@ -318,21 +387,21 @@ void loop_game(t_context *ctx) {
             execute(to_move(key_input), ctx);
             print_tetris(ctx->board, ctx->current, ctx->score);
         }
-        if (has_to_update(ctx->updated_at, FRAME_INTERVAL_USEC)) {
+        if (has_to_fall_by_gravity(ctx->updated_at, ctx->gravity)) {
             execute(MOVE_DOWN, ctx);
             print_tetris(ctx->board, ctx->current, ctx->score);
-            gettimeofday(&ctx->updated_at, NULL);
+			gettimeofday(&ctx->updated_at, NULL);
         }
     }
 }
 
 void destroy_game(t_context *ctx) {
-    destroy_piece(ctx->current);
+    destroy_tetromino(ctx->current);
     endwin();
     print_result(ctx->score, ctx->board);
 }
 
-int main() {
+int main(void) {
     t_context ctx;
 
     init_game(&ctx);
